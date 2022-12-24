9bca7254b52d8f5f7cbc5857586acda7b43293c0
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-24 09:02:05 +0100

    Update pc2-monolitica.py

--- pc2-monolitica.py
+++ pc2-monolitica.py
@@ -5,25 +5,24 @@ import logging
 
 os.system('sudo apt-get update')
 os.system('sudo apt-get install pip -y')
-os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
-os.system('sudo pip install -r ./practica_creativa2/bookinfo/src/productpage/requirements.txt')
+os.system('sudo pip install -r ./CDPSPC2G31/bookinfo/src/productpage/requirements.txt')
 from lxml import etree
 arser = etree.XMLParser(recover=True)
 
 try:
 
     parser = etree.HTMLParser()
-    doc = etree.parse("./practica_creativa2/bookinfo/src/productpage/templates/productpage.html", parser)
+    doc = etree.parse("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", parser)
     etiqueta = doc.find(".//a[@class='navbar-brand']")
     etiqueta.text = "Grupo 31"
 
-    with open("./practica_creativa2/bookinfo/src/productpage/templates/productpage.html", "w") as f:
+    with open("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", "w") as f:
         f.write(etree.tostring(doc, encoding='unicode', pretty_print=True))
 
 except OSError as err:
     print("Error al leer el archivo:", err)
 
-os.system('python3 ./practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 9080')
+os.system('python3 ./CDPSPC2G31/bookinfo/src/productpage/productpage_monolith.py 9080')
 
 
 
