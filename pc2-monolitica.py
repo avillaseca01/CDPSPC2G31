4653580381d0de18d61ed195c804e0edfc5d87ca
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-24 08:57:28 +0100

    Update pc2-monolitica.py

--- pc2-monolitica.py
+++ pc2-monolitica.py
@@ -7,9 +7,6 @@ os.system('sudo apt-get update')
 os.system('sudo apt-get install pip -y')
 os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 os.system('sudo pip install -r ./practica_creativa2/bookinfo/src/productpage/requirements.txt')
-os.system('sudo pip install urllib3==1.21.1')
-os.system('sudo pip install testresources')
-os.system('sudo pip install lxml')
 from lxml import etree
 arser = etree.XMLParser(recover=True)
 
