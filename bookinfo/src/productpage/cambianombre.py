688d73959e2df6d6fd9a685b37e4c326c5880b20
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-26 18:17:02 +0100

    Create cambianombre.py

---
+++ bookinfo/src/productpage/cambianombre.py
@@ -0,0 +1,16 @@
+import os
+import etree from lxml
+ngrupo = os.environ.get("GROUP_NUMBER")
+
+try:
+
+    parser = etree.HTMLParser()
+    doc = etree.parse("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", parser)
+    etiqueta = doc.find(".//a[@class='navbar-brand']")
+    etiqueta.text = "Grupo"+ngrupo
+
+    with open("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", "w") as f:
+        f.write(etree.tostring(doc, encoding='unicode', pretty_print=True))
+
+except OSError as err:
+    print("Error al leer el archivo:", err)
