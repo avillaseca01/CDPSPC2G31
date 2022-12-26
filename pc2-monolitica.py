1d45e7ccc208a92e98de8df75a88369a75f0f132
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-26 17:55:08 +0100

    Add files via upload

--- pc2-monolitica.py
+++ pc2-monolitica.py
@@ -4,25 +4,27 @@ import logging
 
 
 os.system('sudo apt-get update')
+os.environ ["GROUP_NUMBER"]="31"
 os.system('sudo apt-get install pip -y')
-os.system('sudo pip install -r ./bookinfo/src/productpage/requirements.txt')
+os.system('sudo pip install -r ./CDPSPC2G31/bookinfo/src/productpage/requirements.txt')
 from lxml import etree
 arser = etree.XMLParser(recover=True)
+ngrupo = os.environ.get("GROUP_NUMBER")
 
 try:
 
     parser = etree.HTMLParser()
-    doc = etree.parse("./bookinfo/src/productpage/templates/productpage.html", parser)
+    doc = etree.parse("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", parser)
     etiqueta = doc.find(".//a[@class='navbar-brand']")
-    etiqueta.text = "Grupo 31"
+    etiqueta.text = "Grupo"+ngrupo
 
-    with open("./bookinfo/src/productpage/templates/productpage.html", "w") as f:
+    with open("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", "w") as f:
         f.write(etree.tostring(doc, encoding='unicode', pretty_print=True))
 
 except OSError as err:
     print("Error al leer el archivo:", err)
 
-os.system('python3 ./bookinfo/src/productpage/productpage_monolith.py 9080')
+os.system('python3 ./CDPSPC2G31/bookinfo/src/productpage/productpage_monolith.py 9080')
 
 
 
