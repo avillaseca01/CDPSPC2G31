888f8f53124d31da713a0c2f02fd8f082456bac0
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-26 18:57:48 +0100

    Update cambianombre.py

--- bookinfo/src/productpage/cambianombre.py
+++ bookinfo/src/productpage/cambianombre.py
@@ -5,12 +5,12 @@ ngrupo = str(os.environ.get("GROUP_NUMBER"))
 try:
 
     parser = etree.HTMLParser()
-    doc = etree.parse("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", parser)
+    doc = etree.parse("./templates/productpage.html", parser)
     etiqueta = doc.find(".//a[@class='navbar-brand']")
     etiqueta.text = "Grupo"+ngrupo
 
-    with open("./CDPSPC2G31/bookinfo/src/productpage/templates/productpage.html", "w") as f:
+    with open("./templates/productpage.html", "w") as f:
         f.write(etree.tostring(doc, encoding='unicode', pretty_print=True))
 
 except OSError as err:
-    print("Error al leer el archivo:", err)
+    print("Error al leer el archivo:", err))
