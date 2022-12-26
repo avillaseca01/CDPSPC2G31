b1420eda3aaf3557ce7597e0797701e4ce7253af
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-26 19:34:37 +0100

    Update cambianombre.py

--- bookinfo/src/productpage/cambianombre.py
+++ bookinfo/src/productpage/cambianombre.py
@@ -1,13 +1,13 @@
 import os
 from lxml import etree 
-ngrupo = str(os.environ.get("GROUP_NUMBER"))
-
+ngrupoenviron = str(os.environ["GROUP_NUMBER"])
+ngrupogetenv = str(os.getenv("GROUP_NUMBER"))
 try:
 
     parser = etree.HTMLParser()
     doc = etree.parse("./templates/productpage.html", parser)
     etiqueta = doc.find(".//a[@class='navbar-brand']")
-    etiqueta.text = "Grupo"+ngrupo
+    etiqueta.text = "Grupo "+ngrupoenviron+" "+ngrupogetenv
 
     with open("./templates/productpage.html", "w") as f:
         f.write(etree.tostring(doc, encoding='unicode', pretty_print=True))
