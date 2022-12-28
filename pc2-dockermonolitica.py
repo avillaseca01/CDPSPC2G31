552f50919ddc2530fdba75d5ace42be4068faaa8
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-28 09:33:40 +0100

    Delete pc2-dockermonolitica.py

--- pc2-dockermonolitica.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('sudo apt-get update')
-os.system('sudo apt-get install docker.io -y')
-os.system('sudo docker build -t g31/product-page .')
-os.system('sudo docker run --name g31-product-page -p 9080:9080 -e GROUP_NUMBER=31 -d g31/product-page')
