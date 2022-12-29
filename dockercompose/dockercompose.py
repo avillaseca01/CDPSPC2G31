e0501aaa2db902ea8791c6e7d2b168ebeb443ad1
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-29 18:50:42 +0100

    Update dockercompose.py

--- dockercompose/dockercompose.py
+++ dockercompose/dockercompose.py
@@ -1,7 +1,7 @@
 import os
 os.system('sudo apt-get update')
 os.system('sudo apt-get install -y docker.io')
-os.system('sudo apt-get install docker-compose')
+os.system('sudo apt-get install -y docker-compose')
 os.system('cp ./dockercompose/productpage/Dockerfile ./bookinfo/src/productpage/')
 os.system('cp ./dockercompose/details/Dockerfile ./bookinfo/src/details/')
 os.system('cp ./dockercompose/ratings/Dockerfile ./bookinfo/src/ratings/')
