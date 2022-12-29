7e02d21e1d251995e4faf6b7cd399293ea816e26
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2022-12-29 17:16:06 +0100

    Update dockercompose.py

--- dockercompose/dockercompose.py
+++ dockercompose/dockercompose.py
@@ -7,6 +7,7 @@ os.system('cp ./dockercompose/details/Dockerfile ./bookinfo/src/details/')
 os.system('cp ./dockercompose/ratings/Dockerfile ./bookinfo/src/ratings/')
 os.system('cd /bookinfo/src/reviews/reviews-wlpcfg')
 os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w/home/gradle/project gradle:4.8.1 gradle clean build')
+#modificar ruta del Dockerfile de reviews ./reviews/reviews-wlpcfg/servers/LibertyProjectServer
 os.system('cd ..')
 os.system('cd ..')
 os.system('sudo docker-compose up -d')
