86580e1c255d40358610d335b16969fe5b5196fb
paulalopezurq <121335855+paulalopezurq@users.noreply.github.com>
2022-12-29 18:21:18 +0100

    Update dockercompose.py

--- dockercompose/dockercompose.py
+++ dockercompose/dockercompose.py
@@ -5,6 +5,7 @@ os.system('sudo apt-get install docker-compose')
 os.system('cp ./dockercompose/productpage/Dockerfile ./bookinfo/src/productpage/')
 os.system('cp ./dockercompose/details/Dockerfile ./bookinfo/src/details/')
 os.system('cp ./dockercompose/ratings/Dockerfile ./bookinfo/src/ratings/')
+os.system('cp ./dockercompose/docker-compose.yaml ./bookinfo/src/')
 os.system('cd /bookinfo/src/reviews/reviews-wlpcfg')
 os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w/home/gradle/project gradle:4.8.1 gradle clean build')
 #modificar ruta del Dockerfile de reviews ./reviews/reviews-wlpcfg/servers/LibertyProjectServer
