f9160c34c608bd82863e6a6ce2d777d84110c7cd
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2023-01-02 12:24:32 +0100

    Update productpage.py

--- bookinfo/src/productpage/productpage.py
+++ bookinfo/src/productpage/productpage.py
@@ -33,6 +33,21 @@ import logging
 import os
 import asyncio
 
+from lxml import etree 
+ngrupogetenv = os.environ.get("GROUP_NUMBER")
+try:
+
+    parser = etree.HTMLParser()
+    doc = etree.parse("./templates/productpage.html", parser)
+    etiqueta = doc.find(".//a[@class='navbar-brand']")
+    etiqueta.text = "Grupo "+ngrupogetenv
+
+    with open("./templates/productpage.html", "w") as f:
+        f.write(etree.tostring(doc, encoding='unicode', pretty_print=True))
+
+except OSError as err:
+    print("Error al leer el archivo:", err)
+
 # These two lines enable debugging at httplib level (requests->urllib3->http.client)
 # You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
 # The only thing missing will be the response.body which is not logged.
