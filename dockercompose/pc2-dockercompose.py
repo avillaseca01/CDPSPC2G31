95340ee747d3e53d030bb4e9b39062fbeeb41a84
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2023-01-23 22:58:56 +0100

    Update pc2-dockercompose.py

--- dockercompose/pc2-dockercompose.py
+++ dockercompose/pc2-dockercompose.py
@@ -6,7 +6,7 @@ os.system('cp ./dockercompose/productpage/Dockerfile ./bookinfo/src/productpage/
 os.system('cp ./dockercompose/details/Dockerfile ./bookinfo/src/details/')
 os.system('cp ./dockercompose/ratings/Dockerfile ./bookinfo/src/ratings/')
 os.system('cp ./dockercompose/docker-compose.yaml ./bookinfo/src/')
-os.system('cd ./bookinfo/src/reviews/')
+os.chdir(os.getcwd()+'/bookinfo/src/reviews/')
 os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w/home/gradle/project gradle:4.8.1 gradle clean build')
 #modificar ruta del Dockerfile de reviews ./reviews/reviews-wlpcfg/servers/LibertyProjectServer
 os.system('cd ..')
