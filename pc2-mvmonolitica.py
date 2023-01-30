a4efba67a1e332d99783c814cb9d359bb50b2f46
avillaseca01 <121334055+avillaseca01@users.noreply.github.com>
2023-01-30 23:05:41 +0100

    Update pc2-mvmonolitica.py

--- pc2-mvmonolitica.py
+++ pc2-mvmonolitica.py
@@ -7,6 +7,7 @@ os.environ ["GROUP_NUMBER"]="31"
 os.system('sudo apt-get install pip -y')
 os.system('sudo pip install -r ./bookinfo/src/productpage/requirements.txt')
 from lxml import etree
+import re
 ngrupo = os.environ.get("GROUP_NUMBER")
 
 try:
@@ -22,4 +23,11 @@ try:
 except OSError as err:
     print("Error al leer el archivo:", err)
 
+with open("./templates/productpage.html", "r") as f:
+	filedata = f.read()
+
+filedata = re.sub("{% block title %}Simple Bookstore App{% endblock %}", "{% block title %}Grupo "+ngrupogetenv+" {% endblock %}", filedata)
+with open("./templates/productpage.html", "w") as f:
+	f.write(filedata)
+    
 os.system('python3 ./bookinfo/src/productpage/productpage_monolith.py 9080')
